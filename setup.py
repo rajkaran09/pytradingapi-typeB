cf46be68d0d89fd6baa42903dfd97890388b79bc
ishan-mirae <ishan.finquest@miraeassetcm.com>
2025-08-28 11:27:30 +0530

    Updated setup.py

--- setup.py
+++ setup.py
@@ -1,14 +1,15 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="tradingapi",
-    version="0.1.0",
-    author="Nerve Solutions",
-    description="A Python SDK for Connecting to Mirae API and Streaming API",
+    name="mStock-TradingApi-B",
+    version="1.0.0",
+    author="Mirae Asset Capital Markets (India) Private Limited",
+    description="A Python SDK for Connecting to mStock Trading API and Streaming API",
     long_description=open("README.md").read(),
     long_description_content_type="text/markdown",
-    url="",
+    url="https://github.com/MiraeAsset-mStock/pytradingapi-typeB",
     packages=find_packages(),
+    package_dir={'mStock-TradingApi-B': 'tradingapi_b'},
     install_requires=[
         "autobahn", 
         "requests",
@@ -21,6 +22,16 @@ setup(
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
+        "Development Status :: 5 - Production/Stable",
+        "Intended Audience :: Developers",
+        "Intended Audience :: Financial and Insurance Industry",
+        "Natural Language :: English",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Topic :: Office/Business :: Financial :: Investment",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        "Topic :: Software Development :: Libraries"
     ],
     python_requires=">=3.11",
 )
